a8106b836d0f9b3c6d587043aae88a2139a1ee65
JuliaAugustSugarman <jules.august@gmail.com>
2019-03-14 14:41:00 -0500

    add ShelfForm to Home Screen

---
+++ src/components/ShelfForm/ShelfForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+
+class ShelfForm extends Component {
+    // componentDidMount() {
+    //     this.props.dispatch({ type: 'FETCH_ITEM' })
+    // }
+
+    state = {
+        description: '',
+        imageUrl: '',
+
+    };
+
+
+    handleSubmit = () => {
+        console.log('in handleSubmit');
+        this.props.dispatch({
+            type: 'ADD_ITEM',
+            payload: this.state,
+        })
+        this.setState({                        //clear inputs
+            description: '',
+            imageUrl: '',
+        })
+    }
+
+
+    handleChangeFor = (property) => (event) => {  //handling the parameter of property set below 
+        console.log('in handleChange');
+        this.setState({                        //currying
+            ...this.state,
+            [property]: event.target.value,
+        })
+    }
+
+
+
+
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    placeholder="description"
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.handleChangeFor('description')}// event listener
+                />
+                <input
+                    placeholder="imageUrl"
+                    type="text"
+                    value={this.state.imageUrl}
+                    onChange={this.handleChangeFor('imageUrl')}// event listener
+                />
+                <button type="Submit">Add Shelf Item</button>
+            </form>
+
+        )
+    }
+}
+
+
+
+const mapReduxStateToProps = reduxState => {
+    return reduxState;
+};
+
+export default connect(mapReduxStateToProps)(ShelfForm);
